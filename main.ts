88c0f781013c9a30fcc858f5e89190f49299fd12
Tower-IDI <christopher.bass@towerschool.org>
2026-04-22 19:04:26 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+hourOfAi.every(500, function () {
+	
+})
+hourOfAi.onStart(function () {
+	
+})
+hourOfAi.onBumpWall(function () {
+	
+})
+hourOfAi.property(Property.X)
+hourOfAi.arenaProperty(ArenaProperty.Width)
+hourOfAi.doAfter(500, function () {
+	
+})
+hourOfAi.turnBy(90)
+hourOfAi.turnTowards(0)
+hourOfAi.turnTowardsPosition(0, 0)
+hourOfAi.distanceToWall()
+hourOfAi.distanceToColor(ColorType.MyColor)
+hourOfAi.distanceToOpponent()
+hourOfAi.canSeeOpponent()
+hourOfAi.setFillColor(FillColor.Red)
+hourOfAi.setBugColor(ColorTarget.Body, Color.Red)
